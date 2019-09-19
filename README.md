31bcfc2304828c12c7b6f1e82e54f17611a286ca
Yannic Wehner <yannic.wehner@protonmail.com>
2019-09-19 22:17:48 +0200

    Added argument parser...
    ...for better argument parsing, some changes to the UI and other small improvements.

--- README.md
+++ README.md
@@ -3,20 +3,25 @@
 
 ASCIIpy is a simple to use command line tool that helps you to convert an input image into ascii art. For the moment the scrip will print the ascii art to the console (dependent on your src image size you might need to change the text size to something very small if you want to view it in the console) and generate a html and jpg file containing the ascii art all at once for every file you call it with. But I will look into a way how you can call the scrip with some more specific task and more options at hand
 
-## Usage
+## Installation and dependencies
 1. Download the ASCIIpy-master.zip from [here](https://github.com/ElCap1tan/ASCIIpy/archive/master.zip) and unzip it into a location of your liking. I will refer to this location as ```home folder``` from now on.
 2. Place the images you want to convert into the ```in``` folder inside the ```home folder```
-3. Open the terminal and move to the location of the ```ascii.py``` file. If you haven't already you need to install pillow
+3. Open the terminal and move into the the ```home folder```.
+4. Install the dependencies.
 ```
-> pip install Pillow
+> pip install -r requirements.txt
 ```
-4. Run the ```ascii.py``` script where
-* IMAGE_FILE_NAME is the full name of a file in the ```in``` folder for example ```car.jpg```
-* MODE is the way the RGB color values are converted to a single brighness value for each pixel. At the moment this script supports ```lum```inosity, ```avg``` or ```light```ness. The method that gives the best overall results imo is ```lum``` but it might be worth trying the others if you're not okay with the end result.
+## Usage
+Run the ```ascii.py``` script 
 ```
-> python ascii.py IMAGE_FILE_NAME MODE
+> python ascii.py [-h] [-m CONVERSION_MODE] [--rainbow] [--no-console] image
 ```
-5. The output html and jpg files will be written to the ```out``` folder and are named the same as the input file
+where
+* **image** is the full name of a file in the ```in``` folder for example ```car.jpg```
+* **CONVERSION_MODE** is the way the RGB color values are converted to a single brightness value for each pixel. At the moment this script supports ```lum```inosity, ```avg``` or ```light```ness. The method that gives the best overall results imo is ```lum``` but it might be worth trying the others if you're not okay with the end result.
+* The **--rainbow** argument doesn't do anything at the moment. Image will be in grayscale HTML in color by default.
+
+*The output html and jpg files will be written to the ```out``` folder and are named the same as the input file*
 
 ## Examples
 ### Car
--- ascii.py
+++ ascii.py
@@ -1,40 +1,47 @@
+import argparse
 import math
 import sys
 from time import sleep
 
 from PIL import Image, ImageDraw
+from ProgressPrinter import ProgressBar
 import numpy
 
 CONVERSION_STRING_LOW_CONTRAST = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"[::-1]
 CONVERSION_STRING_HIGH_CONTRAST = " .`',°^:;~-_+\"#MW&8%B@$"
 CONVERSION_STRING = " .`',:;~-°^\"_!+\\/mwqp#MW&8%B@$"
 
+DEBUG = False
+
 
 def convert_to_brightness_matrix(rgb_matrix, method):
-    # Use the Luminosity formula to convert RGB values to a single brightness value
+    col_s, row_s = len(rgb_matrix[0]), len(rgb_matrix)  # TODO check if order is right
+    pixel_c = col_s * row_s
     bm = []
-    if method == "lum":  # Luminosity
-        for y in range(0, len(rgb_matrix)):
-            bm.append([])
-            for x in range(0, len(rgb_matrix[y])):
+    pre = f'Converting RGB-Matrix to Brightness-Matrix [RGB ==({method})==> Brightness]...\nPixel matrix size: {col_s} cols x {row_s} rows.'
+    pb = ProgressBar(pixel_c, 'pixels', pre=pre, post='Finished conversion!')
+    pb.print_progress()
+
+    p_count = 0
+
+    # Use the Luminosity formula to convert RGB values to a single brightness value
+    for y in range(0, row_s):
+        bm.append([])
+        for x in range(0, col_s):
+            if method == "lum":  # Luminosity
                 bm[y].append(int((0.21 * rgb_matrix[y, x, 0]) + (0.72 * rgb_matrix[y, x, 1]) +
                                  (0.07 * rgb_matrix[y, x, 2])))
-        return bm
-    elif method == "avg":  # Average
-        for y in range(0, len(rgb_matrix)):
-            bm.append([])
-            for x in range(0, len(rgb_matrix[y])):
+            elif method == "avg":  # Average
                 bm[y].append(int((int(rgb_matrix[y, x, 0]) + int(rgb_matrix[y, x, 1]) + int(rgb_matrix[y, x, 2])) / 3))
-        return bm
-    elif method == "light":  # Lightness
-        for y in range(0, len(rgb_matrix)):
-            bm.append([])
-            for x in range(0, len(rgb_matrix[y])):
+            elif method == "light":  # Lightness
                 bm[y].append(int(int((max(rgb_matrix[y, x, 0], rgb_matrix[y, x, 1], rgb_matrix[y, x, 2])) +
-                                     int(min(rgb_matrix[y, x, 0], rgb_matrix[y, x, 1], rgb_matrix[y, x, 2])) / 2)))
-        return bm
-    else:
-        print("The method {} does't exist.".format(method))
+                             int(min(rgb_matrix[y, x, 0], rgb_matrix[y, x, 1], rgb_matrix[y, x, 2])) / 2)))
+            else:
+                print("The method {} does't exist.".format(method))
+                return -1
+            p_count += 1
+            pb.print_progress(p_count)
+    return bm
 
 
 def downscale_image(image, max_width):
@@ -54,7 +61,6 @@ def create_ascii_matrix(image, max_width, method="luminosity"):
     print("Image size after downscaling: {}x{}".format(image.width, image.height))
 
     pixel_matrix = numpy.asarray(image)
-    print("Pixel matrix size: {} cols x {} rows".format(len(pixel_matrix[0]), len(pixel_matrix)))
 
     brightness_matrix = convert_to_brightness_matrix(pixel_matrix, method)
 
@@ -70,37 +76,64 @@ def create_ascii_matrix(image, max_width, method="luminosity"):
 
     # Convert brightness to ascii chars and append to new matrix
     variance = max_b - min_b
-    print("variance: {}".format(variance))
+    print(f"variance: {variance}")
     m = math.ceil(variance / len(CONVERSION_STRING))  # calculates the brightness range a single char has to cover
-    print("m: {}".format(m))
-
-    sleep(2.75)
+    print(f"Brightness range per char: {m}")
 
     ascii_matrix = []
+    col_s, row_s = len(brightness_matrix[0]), len(brightness_matrix)
+    pixel_c = col_s * row_s
+    pre = f'Converting Brightness-Matrix to ASCII-Matrix using the conversion string\n\n"{CONVERSION_STRING}"\n'
+    pb = ProgressBar(pixel_c, 'pixels', pre=pre, post='Finished conversion!')
+    pb.print_progress()
+
+    p_count = 0
+
     for y in range(0, len(brightness_matrix)):
         ascii_matrix.append([])
         for x in range(0, len(brightness_matrix[y])):
+            p_count += 1
             char_index = round(brightness_matrix[y][x] / m)
             if char_index > len(CONVERSION_STRING) - 1:
-                print("Char index out of bounds: {} => ~{}; The value was corrected to {}"
-                      .format(brightness_matrix[y][x] / m, char_index, len(CONVERSION_STRING) - 1))
+                if DEBUG:
+                    pb.print_progress(p_count, f"Char index out of bounds: {brightness_matrix[y][x] / m} => ~{char_index};"
+                                               f" The value was corrected to {len(CONVERSION_STRING) - 1}")
                 char_index = len(CONVERSION_STRING) - 1
+            else:
+                pb.print_progress(p_count)
             ascii_matrix[y].append(CONVERSION_STRING[char_index])
     return ascii_matrix
 
 
 if __name__ == '__main__':
-    IMAGE = sys.argv[1]
-    METHOD = sys.argv[2]
+    parser = argparse.ArgumentParser(description="Takes in options on how the image should be processed.")
+    parser.add_argument("image", type=str, action="store", default=None, help=
+                        "The full name of the image. The image must be contained"
+                        " inside a folder that is named in that lives in the "
+                        "same location as the ascii.py script.")
+    parser.add_argument("-m", "--mode", type=str,
+                        help="Specifies the mode that is used to convert RGB color values to single "
+                             "brightness values. Modes supported lum[inosity], light[ness], "
+                             "or avg [average]. Defaults to lum", default="lum", dest="conversion_mode")
+    parser.add_argument("--rainbow", action="store_const", const="rainbow", dest="output_mode", default="b/w")
+    parser.add_argument("--no-console", action="store_false", dest="console_flag")
+
+    args = parser.parse_args()
+
+    IMAGE = args.image
+    METHOD = args.conversion_mode
     IN_PATH = "in/" + IMAGE
     OUT_PATH = "out/" + IMAGE
 
     img = Image.open(IN_PATH)
 
     ascii_matrix = create_ascii_matrix(img, 300, METHOD)
+    ascii_count = len(ascii_matrix) * len(ascii_matrix[0])
 
-    if img:
-        f = open((OUT_PATH.split(".")[0] + ".html"), "w+", encoding="utf-8")
+    if ascii_matrix:
+        # SAVE TO HTML
+        file_name = (OUT_PATH.split(".")[0] + ".html")
+        f = open(file_name, "w+", encoding="utf-8")
         f.write('<html style="background-color: #0b0d0a;">\r\n<head>\r\n'
                 '<meta charset="UTF-8">\r\n'
                 '<style>\r\n'  # alt: #664A46 ; #BAA87E
@@ -117,29 +150,52 @@ if __name__ == '__main__':
                 'width: max-content;}\r\n'
                 '</style>\r\n</head>\r\n'
                 '<pre style="font-family: monospace;line-height: 3px; font-size: 3px;" class="rainbow">\r\n')
+
+        pb = ProgressBar(ascii_count, 'characters',
+                         pre=f'Inserting {ascii_count} ASCII characters into {file_name}...',
+                         post='Finished generating HTML file!')
+        pb.print_progress()
+        c = 1
         for r in ascii_matrix:
             line = ""
             for p in r:
                 line += p * 3
+                pb.print_progress(c)
+                c += 1
             f.write(line + "\r\n")
         f.write("</pre>\r\n</html>")
         f.close()
 
-        for r in ascii_matrix:
-            line = ""
-            for p in r:
-                line += p * 2
-            print(line)
+        # SAVE AS NEW ASCII IMAGE
+        file_name = OUT_PATH.split(".")[0] + ".jpg"
         ascii_img = Image.new("RGB", (len(ascii_matrix[0]) * 7, len(ascii_matrix) * 7), (29, 29, 29))
         draw = ImageDraw.Draw(ascii_img)
+        pb = ProgressBar(ascii_count, 'characters',
+                         pre=f'Rendering {ascii_count} ASCII characters onto {file_name}...',
+                         post='Finished generating JPEG file!')
+
         y_position = 0
+        ascii_count = 1
+
+        pb.print_progress()
+
         for l in range(0, len(ascii_matrix)):
             x_position = 0
             y_position = 7 * l
             for r in range(0, len(ascii_matrix[l])):
                 x_position = 7 * r
                 draw.text((x_position, y_position), ascii_matrix[l][r], fill=(255, 213, 175))
-        ascii_img.save(OUT_PATH.split(".")[0] + ".jpg")
+                pb.print_progress(ascii_count)
+                ascii_count += 1
+        ascii_img.save(file_name)
 
+        # PRINT TO CONSOLE
+        if args.console_flag:
+            sleep(1.5)
+            for r in ascii_matrix:
+                line = ""
+                for p in r:
+                    line += p * 2
+                print(line)
     else:
         print("Couldn't load image.")
